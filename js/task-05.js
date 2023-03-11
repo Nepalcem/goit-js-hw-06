6e38f6adb4f818b87896a89e3fa4463a40fed2ce
Nepalcem <mark.emailtest@gmail.com>
2023-03-11 12:45:58 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,7 @@ const inputTag = document.querySelector("#name-input");
 const outputElement = document.querySelector("#name-output");
 
 inputTag.addEventListener("input", (event) => {
+  inputTag.value.trim();
   outputElement.textContent =
     event.currentTarget.value.length > 0
       ? event.currentTarget.value.trim()
